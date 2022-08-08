f9e3b268ebe93f560676eabc0f145d280b408e1b
Vitor Munaretto <vitormunarettoads@hotmail.com>
2022-08-08 09:23:10 -0300

    definido rota /, criado header e footer, criado pagina home, criando formulario de login

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -6,15 +6,9 @@ use \core\Controller;
 class HomeController extends Controller {
 
     public function index() {
-        $this->render('home', ['nome' => 'Bonieky']);
-    }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
+        $_SESSION['title'] = 'Portal';
+        
+        $this->render('home');
     }
 
 }
--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,3 +1,16 @@
 <?php $render('header'); ?>
-
-Opa, <?=$nome;?>
+<header>
+    <div class="dropdown">
+        <button class="dropbtn">Portal de Ensino</button>
+        <form action="" class="drop-content">
+            <label>
+                <input type="text" name="login" placeholder="Codigo ou CPF" />
+            </label><br/>
+            <label>
+                <input type="password" name="password" placeholder="Senha" />
+            </label>
+            <input type="submit" value="Entrar" />
+        </form>
+    </div>
+</header>
+<?php $render('footer'); ?>
